c8a87a763540a2f4a48bf262e1e082f0b14fed37
zanziba <en.mostafa.goravani@gmail.com>
2017-06-11 16:30:10 +0430

    Create plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -22,7 +22,7 @@ local function plugin_exists( name )
 end
 
 local function list_all_plugins(only_enabled)
-  local tmp = '\n\n[BeyondTeam](Telegram.Me/BeyondTeam)'
+  local tmp = '\n\n[linkknab](Telegram.Me/linkknab)'
   local text = ''
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
@@ -68,7 +68,7 @@ local function list_plugins(only_enabled)
      -- text = text..v..'  '..status..'\n'
     end
   end
-  local text = text.."\n_🔃All Plugins Reloaded_\n\n"..nact.." *✔️Plugins Enabled*\n"..nsum.." *📂Plugins Installed*\n\n[BeyondTeam](Telegram.Me/BeyondTeam)"
+  local text = text.."\n_🔃All Plugins Reloaded_\n\n"..nact.." *✔️Plugins Enabled*\n"..nsum.." *📂Plugins Installed*\n\n[linkknab](Telegram.Me/linkknab)"
 return text
 end
 
